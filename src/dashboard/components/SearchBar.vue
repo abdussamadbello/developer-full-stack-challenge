7e5180f000a78b9416e761d69780f778938bf179
Abdussamad  Bello <abdussamadbello4real@hotmail.co.uk>
2023-06-29 22:19:43 +0100

    added search components

---
+++ src/dashboard/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="search-bar">
+        <b-form @submit.prevent="performSearch">
+            <b-input-group>
+                <b-form-input v-model="searchTerm" placeholder="Search by name"></b-form-input>
+                <b-input-group-append>
+                    <b-button type="submit" variant="primary">Search</b-button>
+                </b-input-group-append>
+            </b-input-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchTerm: '',
+        };
+    },
+    methods: {
+        performSearch() {
+            this.$emit('search', this.searchTerm);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.search-bar {
+    margin-bottom: 20px;
+}
+</style>
